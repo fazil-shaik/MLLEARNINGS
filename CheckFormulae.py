356d36015b6ea83500fcbb003a8d24da69ec27dd
Fazil-shaik <ammia4770@gmail.com>
2026-02-18 09:29:55 +0530

    NOn linear model code done random seed

--- CheckFormulae.py
+++ CheckFormulae.py
@@ -4,7 +4,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,mean_squared_error
 import matplotlib.pyplot as plt
-
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import plot_tree
 #linear models 
 
 #formulae = y=mx+b
@@ -50,3 +51,54 @@ print(f"All predictions are {y_pred}")
 # r2 error train data 0.9843833636589749
 
 #no overfitting no underfitting found
+
+#lets check for non linear models
+
+#decision tree regression
+# Split data into regions
+
+# Prediction:
+
+# 𝑦 = Mean of region
+
+np.random.seed(42)
+
+# 1 Feature for visualization clarity
+X = np.sort(5 * np.random.rand(200, 1), axis=0)
+
+# Non-linear relationship
+y = np.sin(X).ravel() + np.random.randn(200) * 0.2
+
+
+#train splitting
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+dt = DecisionTreeRegressor(
+    max_depth=4,        # control tree size
+    min_samples_split=5,
+    min_samples_leaf=3,
+    random_state=42
+)
+
+dt.fit(X_train, y_train)
+
+y_train_pred = dt.predict(X_train)
+y_test_pred = dt.predict(X_test)
+
+print("Train MSE:", mean_squared_error(y_train, y_train_pred))
+print("Test MSE:", mean_squared_error(y_test, y_test_pred))
+
+print("Train R2:", r2_score(y_train, y_train_pred))
+print("Test R2:", r2_score(y_test, y_test_pred))
+
+print("Feature Importance:", dt.feature_importances_)
+
+X_grid = np.arange(0, 5, 0.01).reshape(-1, 1)
+y_grid_pred = dt.predict(X_grid)
+
+plt.scatter(X, y, color="gray", alpha=0.5)
+plt.plot(X_grid, y_grid_pred, color="red")
+plt.title("Decision Tree Regression")
+plt.show()
