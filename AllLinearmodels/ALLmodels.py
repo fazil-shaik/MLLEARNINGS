e4e71108cd7542966a948ca1fec6f98fd67ad66c
Fazil-shaik <ammia4770@gmail.com>
2026-02-22 09:35:20 +0530

    implemented polynomial modell code for linear models

--- AllLinearmodels/ALLmodels.py
+++ AllLinearmodels/ALLmodels.py
@@ -88,3 +88,49 @@ plt.ylabel('Predicted Price')
 plt.title('Multiple Linear Regression: Actual vs Predicted')
 plt.legend()
 plt.show()
+
+#polynomial regression
+
+from sklearn.preprocessing import PolynomialFeatures
+np.random.seed(42)
+
+
+X = np.random.rand(400,3) * 10
+y = 2*X[:,0]**2 + 3*X[:,1]**2 + 4*X[:,2]**2 + np.random.randn(400) * 10
+
+#splitting data
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+#model selection
+PolynomialMOdel = PolynomialFeatures(degree=3)
+X_train_poly = PolynomialMOdel.fit_transform(X_train)
+X_test_poly = PolynomialMOdel.transform(X_test)
+
+#fitting model
+PolyModel = LinearRegression()
+PolyModel.fit(X_train_poly,y_train) 
+
+#predicting model
+y_poly_pred = PolyModel.predict(X_test_poly)
+
+y_test_score = mean_squared_error(y_test,y_poly_pred)
+x_test_score = mean_squared_error(y_test,y_poly_pred)
+
+print(f"Polynomial Regression mse Score: {y_test_score}")
+print(f"Polynomial Regression mse Score: {y_test_score}")
+#model evaluation
+mse_poly = mean_squared_error(y_test,y_poly_pred)
+r2_poly = r2_score(y_test,y_poly_pred)
+print(f"Polynomial Regression Mean Squared Error: {mse_poly}")
+print(f"Polynomial Regression R-squared Score: {r2_poly}")
+
+#plotting model
+plt.figure()
+plt.scatter(y_test, y_poly_pred, label='Predicted vs Actual')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', label='Ideal Fit')
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('Polynomial Regression: Actual vs Predicted')
+plt.legend()
+plt.show()
+
