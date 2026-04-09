304ff234e0ffbfd4869a98206ae004828a6241ed
Fazil-shaik <ammia4770@gmail.com>
2026-04-09 09:25:40 +0530

    Realworld data fetching and not implemented anything just researching

---
+++ realword.py
@@ -0,0 +1,9 @@
+import os
+import requests 
+import json 
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score,accuracy_score,classification_report,mean_squared_error
+
+
