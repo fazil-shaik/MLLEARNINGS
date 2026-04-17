cf5f1dd45b5b8aa627b7305ef5823ad647e15388
Fazil-shaik <ammia4770@gmail.com>
2026-04-17 09:20:32 +0530

    self attention mechanisim with att weights and att res code part done

--- llmmodels.py
+++ llmmodels.py
@@ -163,5 +163,88 @@ print(embedding_layer(torch.tensor(input_id)))
 
 
 
+
+
+
+
 #coding attention mechanisms
 
+
+import torch
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+inputvector = inputs[1]
+print(inputvector)
+input_1 = inputs[0]
+print(input_1)
+
+query = inputs[1]  # 2nd input token is the query
+
+attn_scores_2 = torch.empty(inputs.shape[0])
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query) # dot product (transpose not necessary here since they are 1-dim vectors)
+
+print(attn_scores_2)
+
+
+attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
+
+print("Attention weights:", attn_weights_2_tmp)
+print("Sum:", attn_weights_2_tmp.sum())
+
+
+def sofmax(x):
+    exp_x = torch.exp(x)
+    return exp_x / exp_x.sum()
+attn_weights_2 = sofmax(attn_scores_2)
+print("Softmax Attention weights:", attn_weights_2) 
+
+
+# query = inputs[1]
+
+# context_vector_2 = torch.zeros(inputs.shape[1])
+# for i,x_i in enumerate(inputs):
+#     context_vector_2+=attn_weights_2[i]*x_i
+
+# print("Context vector:", context_vector_2)
+query = inputs[1] # 2nd input token is the query
+
+context_vec_2 = torch.zeros(query.shape)
+for i,x_i in enumerate(inputs):
+    context_vec_2 += attn_weights_2[i]*x_i
+
+print(context_vec_2)
+
+attn_scores = torch.empty(6, 6)
+
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i, j] = torch.dot(x_i, x_j)
+
+print(attn_scores)
+
+
+attn_scores = inputs @ inputs.T
+print(attn_scores)
+
+
+attn_weights = torch.softmax(attn_scores, dim=-1)
+print(attn_weights)
+
+row_2_sum = sum([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
+print("Row 2 sum:", row_2_sum)
+
+print("All row sums:", attn_weights.sum(dim=-1))
+
+all_context_vecs = attn_weights @ inputs
+print(all_context_vecs)
+
+print("Previous 2nd context vector:", context_vec_2)
