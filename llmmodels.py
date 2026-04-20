f0d91e16bde4594620d70ecd91cc17fc6b7656ac
Fazil-shaik <ammia4770@gmail.com>
2026-04-20 09:19:15 +0530

    implementing classic and compact self attention class code done

--- llmmodels.py
+++ llmmodels.py
@@ -250,11 +250,7 @@
 # print("Previous 2nd context vector:", context_vec_2)
 
 
-
-
 #GPT architecture configuration
-
-
 GPT_CONFIG_124M = {
     "vocab_size": 50257,    # Vocabulary size
     "context_length": 1024, # Context length
@@ -340,3 +336,36 @@ model = DummyGPTModel(GPT_CONFIG_124M)
 logits = model(batch)
 print("Output Shape: ",logits.shape)
 print(logits)
+
+
+
+
+
+#implementing classic and compact self attention class
+import torch.nn as nn
+
+class SelfAttention_v1(nn.Module):
+
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+    
