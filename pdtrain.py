3fcd6137582eae225b1d40c6cff51fbd14f10a5f
Fazil-shaik <ammia4770@gmail.com>
2026-04-25 09:27:45 +0530

    Pandas and numpy practice done from code part

---
+++ pdtrain.py
@@ -0,0 +1,78 @@
+# import pandas as pd
+
+
+# # dataframe = pd.read_json('data.json')
+# dataframe = pd.read_csv('data.csv')
+
+# dataframe.dropna(inplace=True)
+
+# # dataframe.fillna(130, inplace = True)
+
+# dataframe.fillna({"Calories": 269}, inplace=True)
+
+# print(dataframe.to_string())
+
+# print(dataframe.head())
+# print(dataframe.tail())
+
+
+
+
+# import pandas as pd
+
+# df = pd.read_csv('./Data.csv')
+
+# # Step 1: Clean column names (remove extra spaces)
+
+# df.drop_duplicates(inplace = True)
+
+
+
+# print(df)
+
+
+# import numpy as np
+
+# arr = np.array([4,5,2,3,1,6])
+
+# arr = np.array((1, 2, 3, 4, 5))
+
+# print(arr)
+
+# arr = np.array(42)
+
+# print(arr)
+
+# arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr)
+
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+# print(arr)
+
+
+
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+# print(arr)
+
+# arr = np.array([1, 2, 3, 4])
+
+# print(arr[1])
+
+# arr = np.array([1, 2, 3, 4])
+
+# print(arr[2] + arr[3])
+
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# print(arr[1:5])
+
+
+
+
+
+#pandas
+
