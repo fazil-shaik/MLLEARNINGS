09d6bc30aad9c2e206f5e5720d7edb190b092a6b
Fazil-shaik <ammia4770@gmail.com>
2026-08-21 10:16:00 +0530

    demo ai project using langchain+langgraph and just runner up

---
+++ genaiproject/dist/main.js
@@ -0,0 +1,8 @@
+import { initChatModel } from "langchain";
+import dotenv from "dotenv";
+dotenv.config();
+const apiKey = process.env.GOOGLE_API_KEY;
+const model = await initChatModel("google-genai:gemini-3.7-flash");
+const response = await model.invoke("Why do parrots talk give me one sentence?");
+console.log(response.content);
+//# sourceMappingURL=main.js.map
