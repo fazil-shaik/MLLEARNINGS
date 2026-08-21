30d8c9c9391232eef08c7ccf0d8632e485cb918c
Fazil-shaik <ammia4770@gmail.com>
2026-08-21 10:19:18 +0530

    removed unwanted stuff

--- genaiproject/dist/main.js
+++
@@ -1,8 +0,0 @@
-import { initChatModel } from "langchain";
-import dotenv from "dotenv";
-dotenv.config();
-const apiKey = process.env.GOOGLE_API_KEY;
-const model = await initChatModel("google-genai:gemini-3.7-flash");
-const response = await model.invoke("Why do parrots talk give me one sentence?");
-console.log(response.content);
-//# sourceMappingURL=main.js.map
